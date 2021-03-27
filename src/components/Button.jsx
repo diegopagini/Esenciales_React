c52798432c339cb550b0d7ce23d0f0dcfe2e99b5
Diego Pagini <dq@qbkconsulting.com>
2021-03-27 06:50:32 -0300

    state

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,14 +2,27 @@
 
 import React from 'react';
 
-const Button = (props) => {
-	const { text } = props;
-	return (
-		<div>
-			<button type='button'>{props.text}</button>
-			<button type='button'>{text}</button>
-		</div>
-	);
-};
+class Button extends React.Component {
+	state = {
+		count: 0,
+	};
+	handleClick = () => {
+		this.setState({
+			count: this.state.count + 1,
+		});
+	};
+
+	render() {
+		const { count } = this.state;
+		return (
+			<div>
+				<h1>Manzanas: {count}</h1>
+				<button onClick={this.handleClick} type='button'>
+					Agregar
+				</button>
+			</div>
+		);
+	}
+}
 
 export default Button;
